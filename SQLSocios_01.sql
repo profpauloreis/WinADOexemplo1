e66473b87325f0a5c805b78d1d45e05a0864dcba
profpauloreis <paulo.reis@aebatalha.ccems.pt>
2024-11-03 11:17:09 +0000

    scripts sql

---
+++ SQLSocios_01.sql
@@ -0,0 +1,105 @@
+﻿/*
+Script de implantação para ClubeXPTOdb2
+
+Este código foi gerado por uma ferramenta.
+As alterações feitas nesse arquivo poderão causar comportamento incorreto e serão perdidas se
+o código for gerado novamente.
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "ClubeXPTOdb2"
+:setvar DefaultFilePrefix "ClubeXPTOdb2"
+:setvar DefaultDataPath "C:\Users\pauloreis\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\"
+:setvar DefaultLogPath "C:\Users\pauloreis\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\"
+
+GO
+:on error exit
+GO
+/*
+Detecta o modo SQLCMD e desabilita a execução do script se o modo SQLCMD não tiver suporte.
+Para reabilitar o script após habilitar o modo SQLCMD, execute o comando a seguir:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'O modo SQLCMD deve ser habilitado para executar esse script com êxito.';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+
+IF (SELECT OBJECT_ID('tempdb..#tmpErrors')) IS NOT NULL DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+GO
+BEGIN TRANSACTION
+GO
+PRINT N'Criando Tabela [dbo].[Socios]...';
+
+
+GO
+CREATE TABLE [dbo].[Socios] (
+    [Id]             INT            IDENTITY (1, 1) NOT NULL,
+    [Nome]           NVARCHAR (100) NOT NULL,
+    [DataNascimento] DATE           NULL,
+    [Email]          NVARCHAR (100) NULL,
+    [Telefone]       NVARCHAR (15)  NULL,
+    [DataEntrada]    DATE           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+IF @@ERROR <> 0
+   AND @@TRANCOUNT > 0
+    BEGIN
+        ROLLBACK;
+    END
+
+IF OBJECT_ID(N'tempdb..#tmpErrors') IS NULL
+    CREATE TABLE [#tmpErrors] (
+        Error INT
+    );
+
+IF @@TRANCOUNT = 0
+    BEGIN
+        INSERT  INTO #tmpErrors (Error)
+        VALUES                 (1);
+        BEGIN TRANSACTION;
+    END
+
+
+GO
+
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT N'A parte transacionada da atualização do banco de dados obteve êxito.'
+COMMIT TRANSACTION
+END
+ELSE PRINT N'A parte transacionada da atualização do banco de dados falhou.'
+GO
+IF (SELECT OBJECT_ID('tempdb..#tmpErrors')) IS NOT NULL DROP TABLE #tmpErrors
+GO
+GO
+PRINT N'Atualização concluída.';
+
+
+GO
---
+++ SQLSocios_INSERT_01.sql
@@ -0,0 +1,8 @@
+﻿INSERT INTO 
+Socios(Nome, DataNascimento, Email, Telefone, DataEntrada) 
+VALUES
+('João Silva', '1990-05-10', 'joao.silva@email.com', 912345678, '2023-01-15'),
+('Maria Oliveira', '1985-12-20', 'maria.oliveira@email.com', 914567890, '2023-03-10'),
+('Pedro Santos', '1992-08-30', 'pedro.santos@email.com', 917654321, '2023-05-05'),
+('Ana Costa', '1988-04-22', 'ana.costa@email.com', 919876543, '2023-07-18'),
+('Carlos Pereira', '1995-11-15', 'carlos.pereira@email.com', 913214567, '2023-09-25');
